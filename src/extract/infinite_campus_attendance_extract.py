12b224070d183bb23458813b9c27ae7bdcdbedb5
Jalen Terrell-Hart <Jalenhart4@gmail.com>
2025-02-26 15:29:31 -0600

    Added git ignore file

--- src/extract/infinite_campus_attendance_extract.py
+++ src/extract/infinite_campus_attendance_extract.py
@@ -7,13 +7,13 @@ def extract_ic_data(ic_attendance_csv):
         personID (int)
         periodID (int) - 17 = AM 18 = PM
         status (char)
-        excuseIDmodifiedDate (Date)
+        modifiedDate (Date)
     """
     # Load the Infinite Campus attendance data into a pandas DataFrame
     ic_df = pd.read_csv(ic_attendance_csv, encoding='utf-8-sig')
 
     # Process the data into a dictionary grouped by personID
-    ic_attendance = ic_df.groupby('personID').apply(lambda x: x[['periodID', 'status', 'excuseIDmodifiedDate']].to_dict(orient='records')).to_dict()
+    ic_attendance = ic_df.groupby('personID').apply(lambda x: x[['periodID', 'status', 'modifiedDate']].to_dict(orient='records')).to_dict()
 
     return ic_attendance
 
